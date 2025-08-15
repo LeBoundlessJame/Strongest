96ef04edbda17811c04b974f98f1138fc5906415
LeBoundlessJame <LeBoundlessJame@users.noreply.github.com>
2025-08-15 23:04:48 +0100

    WOOHOO SMOOTH 'sync'd' flight!

--- src/main/java/com/boundless/ability/reusable_abilities/flight/FlightRendering.java
+++ src/main/java/com/boundless/ability/reusable_abilities/flight/FlightRendering.java
@@ -36,26 +36,30 @@ public class FlightRendering {
         KeybindHoldData forwardData = KeybindingUtils.getHoldData(player, "key.forward");
         KeybindHoldData backData = KeybindingUtils.getHoldData(player, "key.back");
 
+        // Todo: make this follow a tick-based system for speed, etc.
         float rotation = flightRotations.getOrDefault(player, 0f);
         int duration = player.isSprinting() && rotation > 0 ? 15 : 45;
         float clamp = player.isSprinting() ? 1.0f : 0.2f;
         float rotationSpeed = clamp / duration;
 
-        if (player.getAbilities().flying) {
-            if (forwardData.held()) {
-                rotation = MathHelper.clamp(rotation + rotationSpeed, -clamp, clamp);
-            } else if (rotation > 0) {
-                rotation = MathHelper.clamp(rotation - rotationSpeed, -clamp, clamp);
-            } else if (backData.held()) {
-                rotation = MathHelper.clamp(rotation - rotationSpeed, -clamp, 0f);
-            } else if (rotation < 0) {
-                rotation = MathHelper.clamp(rotation + rotationSpeed, -clamp, 0f);
-            }
+        if (forwardData.held()) {
+            rotation = MathHelper.clamp(rotation + rotationSpeed, -clamp, clamp);
+        } else if (rotation > 0) {
+            rotation = MathHelper.clamp(rotation - rotationSpeed, -clamp, clamp);
+        } else if (backData.held()) {
+            rotation = MathHelper.clamp(rotation - rotationSpeed, -clamp, 0f);
+        } else if (rotation < 0) {
+            rotation = MathHelper.clamp(rotation + rotationSpeed, -clamp, 0f);
+        }
 
-            float degrees = rotation * (-90.0F - player.getPitch(tickDelta));
-            matrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(degrees));
+        float degrees = rotation * (-90.0F - player.getPitch(tickDelta));
+        matrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(degrees));
+        flightRotations.put(player, rotation);
 
-            flightRotations.put(player, rotation);
+        if (player.isSprinting() && player.getAbilities().flying) {
+            matrixStack.multiply(RotationAxis.POSITIVE_Y.rotation((float) Math.sin((player.age + tickDelta) * 0.1f) / 5f));
+        } else if (player.getAbilities().flying) {
+            matrixStack.translate(0, (float) Math.sin((player.age + tickDelta) * 0.1f) / 5f, 0);
         }
     }
 }
