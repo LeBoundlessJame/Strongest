b75afc05923c6527f663b4d92faa3c7f8dccb1c5
LeBoundlessJame <LeBoundlessJame@users.noreply.github.com>
2025-08-17 20:26:54 +0100

    Fixed hovering smoothness using hashmap

--- src/main/java/com/boundless/ability/reusable_abilities/flight/FlightRendering.java
+++ src/main/java/com/boundless/ability/reusable_abilities/flight/FlightRendering.java
@@ -1,13 +1,11 @@
 package com.boundless.ability.reusable_abilities.flight;
 
 import com.boundless.ability.components.KeybindHoldData;
-import com.boundless.util.DataComponentUtils;
 import com.boundless.util.HeroUtils;
 import com.boundless.util.KeybindingUtils;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RotationAxis;
 
@@ -15,6 +13,7 @@ import java.util.HashMap;
 
 public class FlightRendering {
     public static HashMap<AbstractClientPlayerEntity, Float> flightRotations = new HashMap<>();
+    public static HashMap<AbstractClientPlayerEntity, Float> playerTicksMap = new HashMap<>();
 
     public static void renderFlight(AbstractClientPlayerEntity player, MatrixStack matrixStack, float f, float g, float tickDelta, float i, PlayerEntityRenderer renderer) {
         KeybindHoldData forwardData = KeybindingUtils.getHoldData(player, "key.forward");
@@ -42,12 +41,14 @@ public class FlightRendering {
         matrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(degrees));
         flightRotations.put(player, rotation);
 
+        float playerTime = playerTicksMap.getOrDefault(player, (float) player.age);
+        playerTime += 1.0f;
+        playerTicksMap.put(player, playerTime);
 
-        float worldTime = (float) player.getWorld().getTime();
         if (player.isSprinting()) {
-            matrixStack.multiply(RotationAxis.POSITIVE_Y.rotation((float) Math.sin((worldTime + tickDelta) * 0.1f) / 5f));
+            matrixStack.multiply(RotationAxis.POSITIVE_Y.rotation((float) Math.sin((playerTime + tickDelta) * 0.025f) / 5f));
         } else {
-            matrixStack.translate(0, (float) Math.sin((worldTime + tickDelta) * 0.1f) / 5f, 0);
+            matrixStack.translate(0, (float) Math.sin((playerTime + tickDelta) * 0.025f) / 5f, 0);
         }
     }
 }
