f00e7501733dd9a8b25b50b72f5c6358440b9329
j <j@noreply.com>
2026-01-07 05:29:49 +0000

    Switcher model added

---
+++ src/main/java/com/boundless/hero/switcher_hero/SwitcherHero.java
@@ -0,0 +1,49 @@
+package com.boundless.hero.switcher_hero;
+
+import com.boundless.BoundlessAPI;
+import com.boundless.ability.AbilityLoadout;
+import com.boundless.hero.api.Hero;
+import com.boundless.hero.api.HeroData;
+import com.boundless.hero.armor.BlackSparksHeroRenderer;
+import com.boundless.hero.armor.HeroArmorRenderer;
+import com.boundless.hero.black_sparks_hero.BlackSparksHUD;
+import com.boundless.hero.black_sparks_hero.BlackSparksHero;
+import com.boundless.registry.AttributeRegistry;
+import net.minecraft.component.type.AttributeModifierSlot;
+import net.minecraft.component.type.AttributeModifiersComponent;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+
+public class SwitcherHero extends Hero {
+
+    public static AttributeModifiersComponent ATTRIBUTES = AttributeModifiersComponent.builder()
+            .add(EntityAttributes.GENERIC_MAX_HEALTH, new EntityAttributeModifier(BoundlessAPI.identifier("generic_max_health"), 20f, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.CHEST)
+            .add(AttributeRegistry.DAMAGE_RESISTANCE, new EntityAttributeModifier(BoundlessAPI.identifier("damage_resistance"), 0.75, EntityAttributeModifier.Operation.ADD_MULTIPLIED_BASE), AttributeModifierSlot.CHEST)
+            .add(EntityAttributes.GENERIC_JUMP_STRENGTH, new EntityAttributeModifier(BoundlessAPI.identifier("generic_jump_strength"), 0.5, EntityAttributeModifier.Operation.ADD_MULTIPLIED_BASE), AttributeModifierSlot.CHEST)
+            .add(EntityAttributes.GENERIC_SAFE_FALL_DISTANCE, new EntityAttributeModifier(BoundlessAPI.identifier("generic_safe_fall_damage_distance"), 35, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.CHEST)
+            .add(AttributeRegistry.TOP_SPEED_MULTIPLIER, new EntityAttributeModifier(BoundlessAPI.identifier("top_speed_multiplier"), 2.5f, EntityAttributeModifier.Operation.ADD_MULTIPLIED_TOTAL), AttributeModifierSlot.CHEST)
+            .add(AttributeRegistry.TIME_UNTIL_MAX_SPEED, new EntityAttributeModifier(BoundlessAPI.identifier("ticks_until_max_speed"), 2, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.CHEST)
+            .build();
+
+    public SwitcherHero() {
+        AbilityLoadout loadout = AbilityLoadout.builder()
+                .ability("key.attack", BlackSparksHero.LIGHT_ATTACK)
+                .ability("key.use", BlackSparksHero.MEDIUM_ATTACK)
+                .ability("key.boundless.ability_one", BlackSparksHero.SPIN_KICK)
+                .build();
+
+        ABILITY_LOADOUTS.put("LOADOUT_1", loadout);
+        this.heroData = HeroData.builder()
+                .name("switcher_hero")
+                .textureIdentifier(BoundlessAPI.textureID("switcher"))
+                .defaultAbilityLoadout(loadout)
+                .attributes(ATTRIBUTES)
+                .hudRenderer(BlackSparksHUD::render)
+                .tickHandler(Hero::heroSprintHandler)
+                .armorRenderer(HeroArmorRenderer::new)
+                .tickHandler(Hero::onHeroTick)
+                .modelIdentifier(BoundlessAPI.modelID("switcher"))
+                .build();
+        this.registerHero();
+    }
+}
--- src/main/java/com/boundless/registry/HeroRegistry.java
+++ src/main/java/com/boundless/registry/HeroRegistry.java
@@ -2,11 +2,13 @@ package com.boundless.registry;
 
 import com.boundless.hero.api.Hero;
 import com.boundless.hero.black_sparks_hero.BlackSparksHero;
+import com.boundless.hero.switcher_hero.SwitcherHero;
 
 import java.util.ArrayList;
 
 public class HeroRegistry {
     public static ArrayList<Hero> HEROES = new ArrayList<>();
     public static Hero BLACK_SPARKS_HERO = new BlackSparksHero();
+    public static Hero SWITCHER_HERO = new SwitcherHero();
     public static void initialize() {}
 }
