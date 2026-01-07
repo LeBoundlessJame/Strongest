57a5ee6c10b754c93a07d8eb402cb11196688a7e
j <j@noreply.com>
2026-01-07 06:15:43 +0000

    Basic switch working

--- src/main/java/com/boundless/hero/switcher_hero/SwitcherHero.java
+++ src/main/java/com/boundless/hero/switcher_hero/SwitcherHero.java
@@ -1,20 +1,43 @@
 package com.boundless.hero.switcher_hero;
 
 import com.boundless.BoundlessAPI;
+import com.boundless.ability.Ability;
 import com.boundless.ability.AbilityLoadout;
 import com.boundless.hero.api.Hero;
 import com.boundless.hero.api.HeroData;
-import com.boundless.hero.armor.BlackSparksHeroRenderer;
 import com.boundless.hero.armor.HeroArmorRenderer;
 import com.boundless.hero.black_sparks_hero.BlackSparksHUD;
 import com.boundless.hero.black_sparks_hero.BlackSparksHero;
 import com.boundless.registry.AttributeRegistry;
+import com.boundless.util.AbilityUtils;
+import com.boundless.util.RaycastUtils;
+import net.minecraft.command.argument.EntityAnchorArgumentType;
 import net.minecraft.component.type.AttributeModifierSlot;
 import net.minecraft.component.type.AttributeModifiersComponent;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.Vec3d;
 
 public class SwitcherHero extends Hero {
+
+    public static Ability BOOGIE = AbilityUtils.ability(SwitcherHero::boogie, 5, BoundlessAPI.identifier("boogie"), BoundlessAPI.hudPNG("clap"));
+
+    public static void boogie(PlayerEntity player) {
+        if (player.getWorld().isClient()) return;
+
+        Entity target = RaycastUtils.thickRaycast(player, 64, 2);
+        if (target == null || target == player || !target.isAlive()) return;
+
+        Vec3d playerPos = player.getPos();
+        Vec3d targetPos = target.getPos();
+
+        target.requestTeleport(playerPos.x, playerPos.y, playerPos.z);
+        player.requestTeleport(targetPos.x, targetPos.y, targetPos.z);
+
+        player.lookAt(EntityAnchorArgumentType.EntityAnchor.FEET, playerPos);
+    }
 
     public static AttributeModifiersComponent ATTRIBUTES = AttributeModifiersComponent.builder()
             .add(EntityAttributes.GENERIC_MAX_HEALTH, new EntityAttributeModifier(BoundlessAPI.identifier("generic_max_health"), 20f, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.CHEST)
@@ -23,11 +46,12 @@ public class SwitcherHero extends Hero {
             .add(EntityAttributes.GENERIC_SAFE_FALL_DISTANCE, new EntityAttributeModifier(BoundlessAPI.identifier("generic_safe_fall_damage_distance"), 35, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.CHEST)
             .add(AttributeRegistry.TOP_SPEED_MULTIPLIER, new EntityAttributeModifier(BoundlessAPI.identifier("top_speed_multiplier"), 2.5f, EntityAttributeModifier.Operation.ADD_MULTIPLIED_TOTAL), AttributeModifierSlot.CHEST)
             .add(AttributeRegistry.TIME_UNTIL_MAX_SPEED, new EntityAttributeModifier(BoundlessAPI.identifier("ticks_until_max_speed"), 2, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.CHEST)
+            .add(EntityAttributes.GENERIC_SCALE, new EntityAttributeModifier(BoundlessAPI.identifier("generic_scale"), 0.1, EntityAttributeModifier.Operation.ADD_MULTIPLIED_BASE), AttributeModifierSlot.CHEST)
             .build();
 
     public SwitcherHero() {
         AbilityLoadout loadout = AbilityLoadout.builder()
-                .ability("key.attack", BlackSparksHero.LIGHT_ATTACK)
+                .ability("key.attack", SwitcherHero.BOOGIE)
                 .ability("key.use", BlackSparksHero.MEDIUM_ATTACK)
                 .ability("key.boundless.ability_one", BlackSparksHero.SPIN_KICK)
                 .build();
