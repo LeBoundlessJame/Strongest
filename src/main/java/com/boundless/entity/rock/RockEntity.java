cdcb3ed87f04a0be445c1a5aaf01b18257efea37
j <j@noreply.com>
2026-01-22 17:40:44 +0000

    Funi rock working ingame

--- src/main/java/com/boundless/entity/rock/RockEntity.java
+++ src/main/java/com/boundless/entity/rock/RockEntity.java
@@ -3,6 +3,8 @@ package com.boundless.entity.rock;
 import com.boundless.action.Action;
 import com.boundless.entity.hero_action.HeroActionEntity;
 import com.boundless.registry.EntityRegistry;
+import mod.azure.azurelib.common.animation.dispatch.command.AzCommand;
+import mod.azure.azurelib.common.animation.play_behavior.AzPlayBehaviors;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -11,6 +13,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class RockEntity extends PersistentProjectileEntity {
+    private static final AzCommand SPIN_COMMAND = AzCommand.create("base_controller",
+            "spin", AzPlayBehaviors.LOOP
+    );
+
     public RockEntity(LivingEntity livingEntity, World world) {
         super(EntityRegistry.ROCK, world);
         this.setOwner(livingEntity);
@@ -20,6 +26,15 @@ public class RockEntity extends PersistentProjectileEntity {
     public RockEntity(EntityType<RockEntity> entityType, World world) {
         super(entityType, world);
         this.pickupType = PickupPermission.DISALLOWED;
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (this.age == 2) {
+            SPIN_COMMAND.sendForEntity(this);
+            System.out.println("Spinning!");
+        }
     }
 
     @Override
--- src/main/java/com/boundless/registry/EntityRenderRegistry.java
+++ src/main/java/com/boundless/registry/EntityRenderRegistry.java
@@ -1,10 +1,12 @@
 package com.boundless.registry;
 
 import com.boundless.entity.hero_action.HeroActionRenderer;
+import com.boundless.entity.rock.RockEntityRenderer;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
 
 public class EntityRenderRegistry {
     public static void initialize() {
         EntityRendererRegistry.INSTANCE.register(EntityRegistry.HERO_ACTION_ENTITY, HeroActionRenderer::new);
+        EntityRendererRegistry.INSTANCE.register(EntityRegistry.ROCK, RockEntityRenderer::new);
     }
 }
