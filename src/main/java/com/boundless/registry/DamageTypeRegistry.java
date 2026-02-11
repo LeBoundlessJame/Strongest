ad83a57edc1355b5a3a65f9991e357e4ff4c9dfa
j <j@noreply.com>
2026-02-11 02:45:25 +0000

    New damage type for cleave

---
+++ src/main/java/com/boundless/registry/DamageTypeRegistry.java
@@ -0,0 +1,14 @@
+package com.boundless.registry;
+
+import com.boundless.BoundlessAPI;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+
+public class DamageTypeRegistry {
+    public static final RegistryKey<DamageType> BYPASS_DEFENCE = registerDamageType("bypass_defence");
+
+    public static RegistryKey<DamageType> registerDamageType(String name) {
+        return RegistryKey.of(RegistryKeys.DAMAGE_TYPE, BoundlessAPI.identifier(name));
+    }
+}
