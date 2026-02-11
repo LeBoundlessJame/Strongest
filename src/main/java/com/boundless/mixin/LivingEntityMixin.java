96aa304b6fb2743ef769d6e96e910ced9c168893
j <j@noreply.com>
2026-02-11 02:59:27 +0000

    Bypass defence should now work

--- src/main/java/com/boundless/mixin/LivingEntityMixin.java
+++ src/main/java/com/boundless/mixin/LivingEntityMixin.java
@@ -1,11 +1,13 @@
 package com.boundless.mixin;
 
 import com.boundless.registry.AttributeRegistry;
+import com.boundless.registry.DamageTypeRegistry;
 import com.boundless.registry.StatusEffectRegistry;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageType;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.registry.entry.RegistryEntry;
@@ -28,15 +30,19 @@ public abstract class LivingEntityMixin {
     @Shadow
     public abstract boolean hasStatusEffect(RegistryEntry<StatusEffect> effect);
 
-    @ModifyReturnValue(method = "modifyAppliedDamage", at = @At("RETURN"))
-    protected float boundless$modifyAppliedDamage(float original) {
+    @Inject(method = "modifyAppliedDamage", at = @At("RETURN"), cancellable = true)
+    protected void boundless$modifyAppliedDamage(DamageSource source, float amount, CallbackInfoReturnable<Float> cir) {
+        if (source.isOf(DamageTypeRegistry.BYPASS_DEFENCE)) cir.setReturnValue(amount);
+
         float damageResistance = (float) (this.getAttributeValue(AttributeRegistry.DAMAGE_RESISTANCE) - 1);
         float resistanceReduction = 0.0f;
+
         if (this.hasStatusEffect(StatusEffectRegistry.BONE_BREAK_EFFECT)) {
             resistanceReduction = (Objects.requireNonNull(this.getStatusEffect(StatusEffectRegistry.BONE_BREAK_EFFECT)).getAmplifier() + 1) * 0.1f;
             resistanceReduction = Math.clamp(resistanceReduction, 0.0f, damageResistance);
         }
-        return (original - (original * (damageResistance - resistanceReduction)));
+
+        cir.setReturnValue (amount - (amount * (damageResistance - resistanceReduction)));
     }
 
     @Inject(method = "createLivingAttributes", at = @At("RETURN"))
