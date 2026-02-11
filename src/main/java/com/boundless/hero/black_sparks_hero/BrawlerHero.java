8240d8eb46ba304f47c27a287669cc6e01da1809
j <j@noreply.com>
2026-02-11 22:06:37 +0000

    Divergent fist now has simpler execution

--- src/main/java/com/boundless/hero/black_sparks_hero/BrawlerHero.java
+++ src/main/java/com/boundless/hero/black_sparks_hero/BrawlerHero.java
@@ -38,6 +38,8 @@ public class BrawlerHero extends Hero {
     public static Ability BLACK_FLASH = AbilityUtils.ability(BrawlerHero::startBlackFlashMinigame, COOLDOWNS.blackFlash.get(), BoundlessAPI.identifier("black_flash"), "Black Flash");
 
     public static Ability LIGHT_ATTACK = AbilityUtils.ability(BrawlerMelee::lightAttack, COOLDOWNS.lightAttack.get(), BoundlessAPI.identifier("brawler_light_attack"), BoundlessAPI.hudPNG("arm"));
+    public static Ability DIVERGENT_FIST = AbilityUtils.ability(BrawlerMelee::divergentFist, COOLDOWNS.blackFlash.get(), BoundlessAPI.identifier("divergent_fist"), BoundlessAPI.hudPNG("arm"), "Divergent Fist");
+
     public static HeldAbility DASH = new DashAbility(DashAbility::chargedLeap, null, COOLDOWNS.dodge.get(), 22, 22, BoundlessAPI.hudPNG("dash"), BoundlessAPI.identifier("dash"), false, 10, "key.boundless.ability_one", "Dash (Hold To Leap)");
 
     public static ComponentType<Long> CHARGED_LEAP_TIME_WINDOW = DataComponentRegistry.registerComponent("charged_leap_time_window", builder -> ComponentType.<Long>builder().codec(Codec.LONG));
@@ -63,6 +65,7 @@ public class BrawlerHero extends Hero {
                 .ability("key.boundless.ability_one", BrawlerHero.DASH)
                 .ability("key.boundless.ability_two", BrawlerHero.SPIN_KICK)
                 .ability("key.boundless.ability_three", BrawlerHero.BLACK_FLASH)
+                .ability("key.boundless.ability_four", BrawlerHero.DIVERGENT_FIST)
                 .build();
 
         ABILITY_LOADOUTS.put("LOADOUT_1", loadout);
--- src/main/java/com/boundless/hero/black_sparks_hero/BrawlerMelee.java
+++ src/main/java/com/boundless/hero/black_sparks_hero/BrawlerMelee.java
@@ -1,11 +1,18 @@
 package com.boundless.hero.black_sparks_hero;
 
 import com.boundless.BoundlessAPI;
+import com.boundless.action.Action;
 import com.boundless.action.Attack;
+import com.boundless.entity.hero_action.HeroActionEntity;
 import com.boundless.registry.SoundRegistry;
-import com.boundless.util.AnimationUtils;
-import com.boundless.util.AttackUtils;
+import com.boundless.util.*;
 import net.minecraft.entity.player.PlayerEntity;
+
+import java.util.LinkedHashMap;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+
+import static com.boundless.hero.black_sparks_hero.BrawlerHero.DAMAGE;
 
 public class BrawlerMelee {
     public static void lightAttack(PlayerEntity player) {
@@ -13,7 +20,7 @@ public class BrawlerMelee {
 
         Attack hook = Attack.builder()
                 .player(player)
-                .damage(BrawlerHero.DAMAGE.lightAttack.get())
+                .damage(DAMAGE.lightAttack.get())
                 .impactSound(SoundRegistry.EARTH_IMPACT)
                 .animationSpeed(1.0f)
                 .animation(BoundlessAPI.identifier("hook"))
@@ -22,6 +29,34 @@ public class BrawlerMelee {
                 .build();
 
         AttackUtils.performAttack(hook);
+    }
+
+    public static void divergentFist(PlayerEntity player) {
+        if (!AttackUtils.canAttack(player)) return;
+
+        LinkedHashMap<Integer, BiConsumer<PlayerEntity, HeroActionEntity>> tasks = new LinkedHashMap<>();
+        tasks.put(4, (user, heroAction) -> {
+            if (CombatUtils.isRolling(player)) return;
+
+            SoundUtils.playSound(player, SoundRegistry.EARTH_IMPACT);
+            CombatUtils.attack(heroAction, DAMAGE.divergentFistPunch.get(), Optional.of(BoundlessAPI.identifier("melee_impact")));
+        });
+        tasks.put(15, (user, heroAction) -> {
+            if (CombatUtils.isRolling(player)) return;
+
+            SoundUtils.playSound(player, SoundRegistry.ENERGY_IMPACT_2);
+
+            CameraUtils.playCameraShake(player);
+            CombatUtils.perEnemyLogic(heroAction, (attacker, livingEntity) -> {
+                livingEntity.timeUntilRegen = 0;
+            });
+            CombatUtils.attack(heroAction, DAMAGE.divergentFistImpact.get(), Optional.of(BoundlessAPI.identifier("divergent_fist_impact")));
+        });
+        Action divergence = Action.builder().scheduledTasks(tasks).build();
+
+        AnimationUtils.playSyncedAnimation(player, BoundlessAPI.identifier("hook"));
+        ActionUtils.performAction(player, divergence);
+        AttackUtils.startAttackTimer(player, 10);
     }
 
     public static void manjiKick(PlayerEntity player) {
