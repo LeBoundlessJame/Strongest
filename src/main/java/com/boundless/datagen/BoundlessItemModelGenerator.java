3c3960d8aca8f89510fb6c3c8cef09b1f304edd6
j <j@noreply.com>
2026-02-26 15:09:50 +0000

    TEMPORARY fix for icons

--- src/main/java/com/boundless/datagen/BoundlessItemModelGenerator.java
+++ src/main/java/com/boundless/datagen/BoundlessItemModelGenerator.java
@@ -23,6 +23,10 @@ public class BoundlessItemModelGenerator extends FabricModelProvider {
     public static final Model LEGGINGS = item("leggings");
     public static final Model BOOTS = item("boots");
 
+    public static final Model MASK_LARGE = item("mask_large");
+    public static final Model CHESTPLATE_LARGE = item("chestplate_large");
+    public static final Model LEGGINGS_LARGE = item("leggings_large");
+    public static final Model BOOTS_LARGE = item("boots_large");
 
     private static Model item(String parent) {
         return new Model(Optional.of(Identifier.of(BoundlessAPI.MOD_ID, "item/" + parent)), null, TextureKey.LAYER0);
@@ -36,12 +40,21 @@ public class BoundlessItemModelGenerator extends FabricModelProvider {
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
         itemModelGenerator.register(ItemRegistry.DEMONIC_FINGER, Models.GENERATED);
 
+        // Todo: this is a small fix just for the update, change this in the future to not be hard coded
         for (Hero hero : HeroRegistry.HEROES) {
             ArrayList<Item> armor = hero.getArmorSet();
-            MASK.upload(ModelIds.getItemModelId(armor.get(0)), heroTexture(hero.heroData.getTextureIdentifier()), itemModelGenerator.writer);
-            CHESTPLATE.upload(ModelIds.getItemModelId(armor.get(1)), heroTexture(hero.heroData.getTextureIdentifier()), itemModelGenerator.writer);
-            LEGGINGS.upload(ModelIds.getItemModelId(armor.get(2)), heroTexture(hero.heroData.getTextureIdentifier()), itemModelGenerator.writer);
-            BOOTS.upload(ModelIds.getItemModelId(armor.get(3)), heroTexture(hero.heroData.getTextureIdentifier()), itemModelGenerator.writer);
+
+            if (hero.heroData.getName().equals("shrine_hero") || hero.heroData.getName().equals("black_sparks_hero")) {
+                MASK_LARGE.upload(ModelIds.getItemModelId(armor.get(0)), heroTexture(hero.heroData.getTextureIdentifier()), itemModelGenerator.writer);
+                CHESTPLATE_LARGE.upload(ModelIds.getItemModelId(armor.get(1)), heroTexture(hero.heroData.getTextureIdentifier()), itemModelGenerator.writer);
+                LEGGINGS_LARGE.upload(ModelIds.getItemModelId(armor.get(2)), heroTexture(hero.heroData.getTextureIdentifier()), itemModelGenerator.writer);
+                BOOTS_LARGE.upload(ModelIds.getItemModelId(armor.get(3)), heroTexture(hero.heroData.getTextureIdentifier()), itemModelGenerator.writer);
+            } else {
+                MASK.upload(ModelIds.getItemModelId(armor.get(0)), heroTexture(hero.heroData.getTextureIdentifier()), itemModelGenerator.writer);
+                CHESTPLATE.upload(ModelIds.getItemModelId(armor.get(1)), heroTexture(hero.heroData.getTextureIdentifier()), itemModelGenerator.writer);
+                LEGGINGS.upload(ModelIds.getItemModelId(armor.get(2)), heroTexture(hero.heroData.getTextureIdentifier()), itemModelGenerator.writer);
+                BOOTS.upload(ModelIds.getItemModelId(armor.get(3)), heroTexture(hero.heroData.getTextureIdentifier()), itemModelGenerator.writer);
+            }
         }
     }
 
