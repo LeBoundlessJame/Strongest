2cba1278b1288470c99cb2a8604f71dea0807144
j <j@noreply.com>
2026-03-07 17:52:07 +0000

    Removed comment

--- src/main/java/com/boundless/combat/CombatSystem.java
+++ src/main/java/com/boundless/combat/CombatSystem.java
@@ -1,15 +1,5 @@
 package com.boundless.combat;
 
 public class CombatSystem {
-    /*
-     * hashmap of target combo and the biconsumer connected to it
-     * evaluate each combo in the hashmap upon each attack
-     * if matches target combo? do combo and reset
-     * else if correct progression? update progress!
-     * else? reset progress on combo
-     * example: for combo component in hashmap
-     * HashMap<Combo string, BiConsumer<PlayerEntity, HeroAction>
-     * lllmll_progress = "l"
-     * store as components on player, which are generated from hashmap
-     */
+
 }
