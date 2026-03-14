5cba8389f4b1c75fe611ce1eebbfe66d58404af3
j <j@noreply.com>
2026-03-14 20:30:54 +0000

    CE working for sukuna

--- src/main/java/com/boundless/util/MeterUtils.java
+++ src/main/java/com/boundless/util/MeterUtils.java
@@ -11,7 +11,9 @@ public class MeterUtils {
     public static void consumeMeter(PlayerEntity player, float percentage) {
         float meterRemaining = HeroUtils.getHeroStack(player).getOrDefault(DataComponentRegistry.METER, 1.0f);
         if (meterRemaining - percentage >= 0.0){
-            HeroUtils.getHeroStack(player).set(DataComponentRegistry.METER, meterRemaining - percentage);
+            float updatedMeterValue = meterRemaining - percentage;
+            updatedMeterValue = Math.round(updatedMeterValue * 100f) / 100f;
+            HeroUtils.getHeroStack(player).set(DataComponentRegistry.METER, updatedMeterValue);
         }
     }
 }
