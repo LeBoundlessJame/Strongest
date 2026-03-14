7270470458e4ab6d3d96bc7ed9eaa759a2001dbf
j <j@noreply.com>
2026-03-14 18:17:50 +0000

    Cooking on side roll

--- src/main/java/com/boundless/networking/payloads/evasion/EvasionServerPayload.java
+++ src/main/java/com/boundless/networking/payloads/evasion/EvasionServerPayload.java
@@ -21,27 +21,22 @@ public record EvasionServerPayload(String direction) implements CustomPayload {
 
     public static void receive(EvasionServerPayload payload, ServerPlayNetworking.Context context) {
         PlayerEntity player = context.player();
+        String direction = payload.direction;
+
         player.setFireTicks(0);
 
         Entity raycastEntity = CombatSystem.getRaycastEntity(player, 32, 1.5f);
-
-        if (payload.direction.equals("forward") && raycastEntity != null && raycastEntity.distanceTo(player) > 6) {
+        if (direction.equals("forward") && raycastEntity != null && raycastEntity.distanceTo(player) > 6) {
             MeleeAbilities.offensiveDash(player);
             return;
         }
 
-        String animation = payload.direction.equals("back") ? "roll_back" : (player.isOnGround() ? "front_handspring" : "roll_forward");
-        float animationSpeed = payload.direction.equals("back") ? 1.25f : (player.isOnGround() ? 1.75f : 1.0f);
-        AnimationUtils.playSyncedAnimation(player, BoundlessAPI.identifier(animation), animationSpeed, false, true, 9999);
-
-        float groundRollDist = 1.5f;
-        Vec3d normalizedRotationVector = player.getRotationVector().normalize().multiply(groundRollDist);
-
-        if (payload.direction.equals("back")) {
-            normalizedRotationVector = normalizedRotationVector.multiply(-1);
+        if (direction.equals("right") || direction.equals("left")) {
+            sideDodge(player, direction, 1.5f);
+        } else {
+            dodgeRoll(player, direction, 1.5f);
         }
 
-        player.addVelocity(new Vec3d(normalizedRotationVector.x, normalizedRotationVector.y, normalizedRotationVector.z));
         player.velocityDirty = true;
         player.velocityModified = true;
     }
@@ -50,4 +45,27 @@ public record EvasionServerPayload(String direction) implements CustomPayload {
     public Id<? extends CustomPayload> getId() {
         return ID;
     }
+
+    public static void sideDodge(PlayerEntity player, String direction, float distance) {
+        Vec3d side = player.getRotationVector().crossProduct(new Vec3d(0, 1, 0))
+                .normalize().multiply(distance);
+
+        if (direction.equals("left")) {
+            side = side.multiply(-1);
+        }
+
+        player.addVelocity(side.x, 0, side.z);
+    }
+
+    public static void dodgeRoll(PlayerEntity player, String direction, float distance) {
+        String animation = direction.equals("back") ? "roll_back" : (player.isOnGround() ? "front_handspring" : "roll_forward");
+        float animationSpeed = direction.equals("back") ? 1.25f : (player.isOnGround() ? 1.75f : 1.0f);
+        AnimationUtils.playSyncedAnimation(player, BoundlessAPI.identifier(animation), animationSpeed, false, true, 9999);
+
+        Vec3d normalizedRotationVector = player.getRotationVector().normalize()
+                .multiply(distance).multiply(direction.equals("back") ? -1 : 1);
+
+        player.addVelocity(new Vec3d(normalizedRotationVector.x, normalizedRotationVector.y, normalizedRotationVector.z));
+    }
+
 }
