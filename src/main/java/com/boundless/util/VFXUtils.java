bbfcd21e0d7bf0b50958b6000c9387ca4d68196a
j <j@noreply.com>
2026-03-19 02:13:37 +0000

    WIP createEffectInstance method

--- src/main/java/com/boundless/util/VFXUtils.java
+++ src/main/java/com/boundless/util/VFXUtils.java
@@ -2,11 +2,27 @@ package com.boundless.util;
 
 import com.boundless.BoundlessAPI;
 import mod.chloeprime.aaaparticles.api.client.effekseer.ParticleEmitter;
+import mod.chloeprime.aaaparticles.api.common.AAALevel;
+import mod.chloeprime.aaaparticles.api.common.ParticleEmitterInfo;
 import mod.chloeprime.aaaparticles.client.registry.EffectRegistry;
+import net.minecraft.entity.Entity;
 
 import java.util.Optional;
 
 public class VFXUtils {
+    /** Creates (and spawns) an effect instance
+     * @param effectName the name of the effek file
+     * @param id can be an entity ID for example; something unique to identify "this" version of the particle
+     */
+    public void createEffectInstance(Entity user, String effectName, int id) {
+        ParticleEmitterInfo particleEmitter = ParticleEmitterInfo.create(user.getWorld(), BoundlessAPI.identifier(effectName), BoundlessAPI.identifier(effectName + id));
+        /*
+        particleEmitter.position(pos);
+        particleEmitter.scale(scale);
+         */
+        AAALevel.addParticle(user.getWorld(), true, particleEmitter);
+    }
+
     /**
      * @param effectName the name of the effek file
      * @param id can be an entity ID for example; something unique to identify "this" version of the particle
