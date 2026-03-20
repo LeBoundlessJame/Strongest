8b634178740924216ad016c09c82d1ba8fae64f7
j <j@noreply.com>
2026-03-20 02:47:17 +0000

    Added CE components

--- src/main/java/com/boundless/registry/StrongestComponents.java
+++ src/main/java/com/boundless/registry/StrongestComponents.java
@@ -5,5 +5,7 @@ import net.minecraft.component.ComponentType;
 
 public class StrongestComponents {
     public static ComponentType<Float> BLACK_FLASH_CHANCE = DataComponentRegistry.registerComponent("black_flash_chance", builder -> ComponentType.<Float>builder().codec(Codec.FLOAT));
+    public static ComponentType<Integer> MAX_CURSED_ENERGY = DataComponentRegistry.registerComponent("max_cursed_energy", builder -> ComponentType.<Integer>builder().codec(Codec.INT));
+    public static ComponentType<Integer> CURSED_ENERGY = DataComponentRegistry.registerComponent("cursed_energy", builder -> ComponentType.<Integer>builder().codec(Codec.INT));
     public static void initialize() {}
 }
