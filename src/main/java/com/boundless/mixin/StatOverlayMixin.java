27592b41a0e3294ea8fb8025fcd54f2ba5ca2e8b
j <j@noreply.com>
2026-03-20 17:27:53 +0000

    Cancelled hotbar rendering in combat mode

--- src/main/java/com/boundless/mixin/StatOverlayMixin.java
+++ src/main/java/com/boundless/mixin/StatOverlayMixin.java
@@ -5,6 +5,7 @@ import com.boundless.util.HeroUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -27,5 +28,17 @@ public class StatOverlayMixin {
         StatOverlays.renderCursedEnergyOverlay(client, context);
         StatOverlays.renderCombatModeIndicator(client, context);
         StatOverlays.renderBlockIndicator(client, context);
+    }
+
+    @Inject(method = "renderHotbar", at = @At("HEAD"), cancellable = true)
+    public void boundless$cancelHotbar(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        if (!HeroUtils.isHero(client.player) || client.player == null) return;
+        if (HeroUtils.combatModeEnabled(client.player) && !client.player.isInCreativeMode()) ci.cancel();
+    }
+
+    @Inject(method = "renderExperienceBar", at = @At("HEAD"), cancellable = true)
+    public void boundless$cancelHotbar(DrawContext context, int x, CallbackInfo ci) {
+        if (!HeroUtils.isHero(client.player) || client.player == null) return;
+        if (HeroUtils.combatModeEnabled(client.player) && !client.player.isInCreativeMode()) ci.cancel();
     }
 }
