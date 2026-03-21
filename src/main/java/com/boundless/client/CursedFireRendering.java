fa2f54c71cb68b086d0200a5cf0343e4186ddfac
j <j@noreply.com>
2026-03-21 20:29:13 +0000

    This took WAY too long omg

--- src/main/java/com/boundless/client/CursedFireRendering.java
+++ src/main/java/com/boundless/client/CursedFireRendering.java
@@ -1,44 +1,50 @@
 package com.boundless.client;
 
+import com.boundless.BoundlessAPI;
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.model.ModelLoader;
-import net.minecraft.client.texture.Sprite;
-import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.Identifier;
 import org.joml.Quaternionf;
 
 public class CursedFireRendering {
 
+    // This took WAY too long lmao
     public static void renderCursedFire(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity, Quaternionf rotation) {
-        Sprite sprite = ModelLoader.FIRE_0.getSprite();
-        Sprite sprite2 = ModelLoader.FIRE_1.getSprite();
+        Identifier fire0 = BoundlessAPI.identifier("textures/block/cursed_fire_0.png");
+        Identifier fire1 = BoundlessAPI.identifier("textures/block/cursed_fire_1.png");
+
+        int totalFrames = 32;
+        float frameProgress = entity.age % totalFrames;
+        float vStep = 16f / 512f;
+        float vMin = frameProgress * vStep;
+        float vMax = vMin + vStep;
+
         matrices.push();
-        float f = entity.getWidth() * 2F;
+        float f = entity.getWidth() * 2f;
         matrices.scale(f, f, f);
         float g = 0.5F;
-        float h = 0.0F;
-        float i = entity.getHeight() / f;
         float j = 0.0F;
+        float i = entity.getHeight() / f;
         matrices.multiply(rotation);
-        matrices.translate(0.0F, 0.0F, 0.3F - (int)i * 0.02F);
+        matrices.translate(0.0F, 0.0F, 0.3F - (int) i * 0.02F);
         float k = 0.0F;
         int l = 0;
-        VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE));
 
         for (MatrixStack.Entry entry = matrices.peek(); i > 0.0F; l++) {
-            Sprite sprite3 = l % 2 == 0 ? sprite : sprite2;
-            float m = sprite3.getMinU();
-            float n = sprite3.getMinV();
-            float o = sprite3.getMaxU();
-            float p = sprite3.getMaxV();
+            Identifier texture = l % 2 == 0 ? fire0 : fire1;
+            VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(texture));
+
+            float uMin = 0.0F;
+            float uMax = 1.0F;
+
             if (l / 2 % 2 == 0) {
-                float q = o;
-                o = m;
-                m = q;
+                float temp = uMax;
+                uMax = uMin;
+                uMin = temp;
             }
 
             float red = 0.2f;
@@ -46,10 +52,11 @@ public class CursedFireRendering {
             float blue = 1.0f;
             float alpha = 0.4f;
 
-            drawCursedFireVertex(entry, vertexConsumer, -g - 0.0F, 0.0F - j, k, o, p, red, green, blue, alpha);
-            drawCursedFireVertex(entry, vertexConsumer, g - 0.0F, 0.0F - j, k, m, p, red, green, blue, alpha);
-            drawCursedFireVertex(entry, vertexConsumer, g - 0.0F, 1.4F - j, k, m, n, red, green, blue, alpha);
-            drawCursedFireVertex(entry, vertexConsumer, -g - 0.0F, 1.4F - j, k, o, n, red, green, blue, alpha);
+            drawCursedFireVertex(entry, vertexConsumer, -g, -j, k, uMax, vMax, red, green, blue, alpha);
+            drawCursedFireVertex(entry, vertexConsumer, g, -j, k, uMin, vMax, red, green, blue, alpha);
+            drawCursedFireVertex(entry, vertexConsumer, g, 1.4F - j, k, uMin, vMin, red, green, blue, alpha);
+            drawCursedFireVertex(entry, vertexConsumer, -g, 1.4F - j, k, uMax, vMin, red, green, blue, alpha);
+
             i -= 0.45F;
             j -= 0.45F;
             g *= 0.9F;
@@ -58,7 +65,6 @@ public class CursedFireRendering {
 
         matrices.pop();
     }
-
 
     private static void drawCursedFireVertex(MatrixStack.Entry entry, VertexConsumer vertices, float x, float y, float z, float u, float v, float red, float green, float blue, float alpha) {
         vertices.vertex(entry, x, y, z)
