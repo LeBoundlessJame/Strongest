34bd574ed2d4fa325faea465ce87aa725190747d
j <j@noreply.com>
2026-03-21 21:20:41 +0000

    Cursed energy fire mixin working!

--- src/main/java/com/boundless/mixin/CursedEnergyFireMixin.java
+++ src/main/java/com/boundless/mixin/CursedEnergyFireMixin.java
@@ -5,18 +5,24 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.MathHelper;
 import org.joml.Quaternionf;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderDispatcher.class)
 public class CursedEnergyFireMixin {
+    @Shadow
+    private Quaternionf rotation;
 
-    @Inject(method = "renderFire", at = @At("HEAD"), cancellable = true)
-    private void boundless$renderFire(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity, Quaternionf rotation, CallbackInfo ci) {
-        CursedFireRendering.renderAllCursedFire(matrices, vertexConsumers, entity, rotation);
-        ci.cancel();
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;pop()V", shift = At.Shift.BEFORE))
+    private void boundless$renderCursedFire(Entity entity, double x, double y, double z, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        if (entity instanceof PlayerEntity player && player.isSneaking()) {
+            CursedFireRendering.renderAllCursedFire(matrices, vertexConsumers, entity, MathHelper.rotateAround(MathHelper.Y_AXIS, this.rotation, new Quaternionf()));
+        }
     }
 }
