bb35e80d2b2d7c932ba82f0c058fadb98ca51b23
j <j@noreply.com>
2026-03-22 00:48:04 +0000

    Simple domain should now break if low

--- src/main/java/com/boundless/ability/SimpleDomain.java
+++ src/main/java/com/boundless/ability/SimpleDomain.java
@@ -11,6 +11,9 @@ public class SimpleDomain {
         if (!isSimpleDomainActive(player)) return;
 
         MeleeUtils.disorientWithExemption(player, 2, BoundlessAPI.identifier("simple_domain"));
+        if (HeroUtils.getHeroStack(player).getOrDefault(StrongestComponents.SIMPLE_DOMAIN_HEALTH, 0.0f) <= 0.0f) {
+            breakSimpleDomain(player);
+        }
         //DataComponentUtils.incrementInt(StrongestComponents.SIMPLE_DOMAIN_HEALTH, player, -1, 0, 150);
     }
 
@@ -24,10 +27,14 @@ public class SimpleDomain {
         if (DataComponentUtils.toggleBoolean(player, StrongestComponents.SIMPLE_DOMAIN_ACTIVE)) {
             AnimationUtils.playSyncedAnimation(player, BoundlessAPI.identifier("simple_domain"));
             VFXUtils.createAndSpawnEffectInstance(player, "simple_domain", null, null, true);
-            HeroUtils.getHeroStack(player).set(StrongestComponents.SIMPLE_DOMAIN_HEALTH, 150);
+            HeroUtils.getHeroStack(player).set(StrongestComponents.SIMPLE_DOMAIN_HEALTH, 150f);
             return;
         }
 
+        breakSimpleDomain(player);
+    }
+
+    public static void breakSimpleDomain(PlayerEntity player) {
         VFXUtils.destroyEffectInstance("simple_domain", player.getId());
     }
 }
