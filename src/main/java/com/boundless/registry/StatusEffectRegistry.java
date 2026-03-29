59028413242057f702e4107775a64f608fb81323
j <j@noreply.com>
2026-03-29 23:53:09 +0100

    Added basic zone effect

--- src/main/java/com/boundless/registry/StatusEffectRegistry.java
+++ src/main/java/com/boundless/registry/StatusEffectRegistry.java
@@ -9,22 +9,23 @@ import net.minecraft.registry.Registry;
 import net.minecraft.registry.entry.RegistryEntry;
 
 public class StatusEffectRegistry {
-    public static final RegistryEntry<StatusEffect> INVULNERABILITY_EFFECT = registerStatusEffect("invulnerability");
-    public static final RegistryEntry<StatusEffect> IMPACT_FRAME_EFFECT = registerStatusEffect("impact_frame_effect");
-    public static final RegistryEntry<StatusEffect> CLAP_IMPACT_FRAME_EFFECT = registerStatusEffect("clap_impact_frame_effect");
-    public static final RegistryEntry<StatusEffect> BONE_BREAK_EFFECT = registerStatusEffect("bone_break_effect");
-    public static final RegistryEntry<StatusEffect> CINEMATIC_BARS = registerStatusEffect("cinematic_bars_effect");
-    public static final RegistryEntry<StatusEffect> SHRINE_EFFECT = registerStatusEffect("shrine_effect");
-    public static final RegistryEntry<StatusEffect> GRAYSCALE = registerStatusEffect("grayscale_effect");
-    public static final RegistryEntry<StatusEffect> LIMITED_SPEED = registerStatusEffect("limited_speed");
-    public static final RegistryEntry<StatusEffect> GUARD_BREAK = registerStatusEffect("guard_break");
-    public static final RegistryEntry<StatusEffect> STUN = registerStatusEffect("stun", new StunEffect(StatusEffectCategory.HARMFUL, 0xffffff));
+    public static final RegistryEntry<StatusEffect> INVULNERABILITY_EFFECT = registerGenericEffect("invulnerability");
+    public static final RegistryEntry<StatusEffect> IMPACT_FRAME_EFFECT = registerGenericEffect("impact_frame_effect");
+    public static final RegistryEntry<StatusEffect> CLAP_IMPACT_FRAME_EFFECT = registerGenericEffect("clap_impact_frame_effect");
+    public static final RegistryEntry<StatusEffect> BONE_BREAK_EFFECT = registerGenericEffect("bone_break_effect");
+    public static final RegistryEntry<StatusEffect> CINEMATIC_BARS = registerGenericEffect("cinematic_bars_effect");
+    public static final RegistryEntry<StatusEffect> SHRINE_EFFECT = registerGenericEffect("shrine_effect");
+    public static final RegistryEntry<StatusEffect> GRAYSCALE = registerGenericEffect("grayscale_effect");
+    public static final RegistryEntry<StatusEffect> LIMITED_SPEED = registerGenericEffect("limited_speed");
+    public static final RegistryEntry<StatusEffect> GUARD_BREAK = registerGenericEffect("guard_break");
+    public static final RegistryEntry<StatusEffect> STUN = registerGenericEffect("stun", new StunEffect(StatusEffectCategory.HARMFUL, 0xffffff));
+    public static final RegistryEntry<StatusEffect> ZONE = registerGenericEffect("zone");
 
-    private static RegistryEntry<StatusEffect> registerStatusEffect(String name) {
+    private static RegistryEntry<StatusEffect> registerGenericEffect(String name) {
         return Registry.registerReference(Registries.STATUS_EFFECT, BoundlessAPI.identifier(name), new GenericStatusEffect(StatusEffectCategory.NEUTRAL, 0xffffff));
     }
 
-    private static RegistryEntry<StatusEffect> registerStatusEffect(String name, StatusEffect statusEffect) {
+    private static RegistryEntry<StatusEffect> registerGenericEffect(String name, StatusEffect statusEffect) {
         return Registry.registerReference(Registries.STATUS_EFFECT, BoundlessAPI.identifier(name), statusEffect);
     }
 
