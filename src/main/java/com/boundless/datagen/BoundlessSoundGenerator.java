fc2418e5515878e1555af3a44d59e3e6ac0fd061
j <j@noreply.com>
2026-03-29 19:13:05 +0100

    Added all the sounds to the generator

--- src/main/java/com/boundless/datagen/BoundlessSoundGenerator.java
+++ src/main/java/com/boundless/datagen/BoundlessSoundGenerator.java
@@ -14,5 +14,18 @@ public class BoundlessSoundGenerator extends SoundEventProvider {
     @Override
     public void registerSounds(BiConsumer<String, Supplier<JsonElement>> consumer) {
         add("impact_heavy_1", consumer);
+        add("miss_hit", consumer);
+        add("clap_1", consumer);
+        add("energy_impact_1", consumer);
+        add("energy_impact_2", consumer);
+        add("energy_impact_3", consumer);
+        add("energy_impact_heavy", consumer);
+        add("earth_impact", consumer);
+        add("rock_crumbling", consumer);
+        add("heavy_cut_1", consumer);
+        add("heavy_cut_2", consumer);
+        add("heavy_cut_3", consumer);
+        add("slash_1", consumer);
+        add("slash_2", consumer);
     }
 }
