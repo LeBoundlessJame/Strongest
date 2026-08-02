6df8e2980dd16eb73e577e4540ee5b50e64fb27e
j <j@noreply.com>
2026-08-02 11:28:01 +0100

    Fixed kuro animation by exporting to geckolib instead of azurelib format

--- src/main/java/com/boundless/entity/divine_dogs/kuro/DivineDogKuroAnimator.java
+++ src/main/java/com/boundless/entity/divine_dogs/kuro/DivineDogKuroAnimator.java
@@ -5,7 +5,6 @@ import mod.azure.azurelib.common.animation.controller.AzAnimationController;
 import mod.azure.azurelib.common.animation.controller.AzAnimationControllerContainer;
 import mod.azure.azurelib.common.animation.impl.AzEntityAnimator;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
 import org.jetbrains.annotations.NotNull;
 
 public class DivineDogKuroAnimator extends AzEntityAnimator<DivineDogKuroEntity> {
@@ -18,34 +17,5 @@ public class DivineDogKuroAnimator extends AzEntityAnimator<DivineDogKuroEntity>
     @Override
     public @NotNull Identifier getAnimationLocation(DivineDogKuroEntity animatable) {
         return BoundlessAPI.identifier("animations/entity/divine_dog.animation.json");
-    }
-
-    @Override
-    public void setCustomAnimations(DivineDogKuroEntity animatable, float partialTicks) {
-        super.setCustomAnimations(animatable, partialTicks);
-
-        if (animatable.isInSittingPose()) return;
-
-        var model = this.context().boneCache().getBakedModel();
-
-        //var tail = model.getBone("tail");
-
-        var left_front = model.getBone("leg1");
-        var right_front = model.getBone("leg2");
-        var left_back = model.getBone("leg3");
-        var right_back = model.getBone("leg4");
-
-        float swing = animatable.limbAnimator.getPos(partialTicks);
-        float amount = animatable.limbAnimator.getSpeed(partialTicks);
-
-        float walkAngle = swing * 0.6662F;
-
-        float right = MathHelper.cos(walkAngle) * 1.4F * amount;
-        float left = MathHelper.cos(walkAngle + MathHelper.PI) * 1.4F * amount;
-
-        left_front.ifPresent(bone -> bone.setRotX(right));
-        right_front.ifPresent(bone -> bone.setRotX(left));
-        left_back.ifPresent(bone -> bone.setRotX(left));
-        right_back.ifPresent(bone -> bone.setRotX(right));
     }
 }
--- src/main/java/com/boundless/entity/divine_dogs/kuro/DivineDogKuroEntity.java
+++ src/main/java/com/boundless/entity/divine_dogs/kuro/DivineDogKuroEntity.java
@@ -1,5 +1,6 @@
 package com.boundless.entity.divine_dogs.kuro;
 
+import mod.azure.azurelib.common.util.MoveAnalysis;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
@@ -9,33 +10,60 @@ import net.minecraft.world.World;
 
 public class DivineDogKuroEntity extends WolfEntity {
     public final DivineDogKuroDispatcher dispatcher;
+    public final MoveAnalysis moveAnalysis;
 
     public DivineDogKuroEntity(EntityType<? extends WolfEntity> entityType, World world) {
         super(entityType, world);
         this.dispatcher = new DivineDogKuroDispatcher(this);
+        this.moveAnalysis = new MoveAnalysis(this);
     }
 
     @Override
     public void tick() {
         super.tick();
+        moveAnalysis.update();
         this.animationTick();
     }
 
     public void animationTick() {
         if (!this.getWorld().isClient) return;
 
-        if (this.isInSittingPose()) {
-            this.dispatcher.layIdle();
-        } else {
-            this.dispatcher.idle();
+        boolean isMovingOnGround = this.moveAnalysis.isMovingHorizontally() && this.isOnGround();
+
+        this.dispatcher.run();
+
+        /*
+        if (isMovingOnGround) {
+            if (this.hasAngerTime()) {
+                this.dispatcher.run();
+            }
         }
+
+         */
+
+        /*
+        if (!this.isInSittingPose()) {
+
+            if (isMovingOnGround) {
+                if (this.hasAngerTime()) {
+                    this.dispatcher.run();
+                    System.out.println("Angry boi");
+                } else {
+                    System.out.println("Calm boi");
+                    this.dispatcher.walk();
+                }
+            } else {
+                //this.dispatcher.idle();
+            }
+        } else {
+            this.dispatcher.layIdle();
+        }
+
+         */
     }
 
 
     public static DefaultAttributeContainer.Builder createWolfAttributes() {
-        return MobEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.45F)
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 100.0)
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 12.0);
+        return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.45F).add(EntityAttributes.GENERIC_MAX_HEALTH, 100.0).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 12.0);
     }
 }
