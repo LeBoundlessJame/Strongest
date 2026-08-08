33c351455276075a6368cabd6fece896dcdb8fb3
j <j@noreply.com>
2026-08-08 16:18:57 +0100

    Shikigami toggle working / cleaned up, still requires death handling but works

--- src/main/java/com/boundless/util/ShikigamiUtils.java
+++ src/main/java/com/boundless/util/ShikigamiUtils.java
@@ -1,11 +1,11 @@
 package com.boundless.util;
 
 import com.boundless.hero.shadow_hero.ShadowHero;
-import kotlin.uuid.Uuid;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
 
@@ -18,85 +18,42 @@ public class ShikigamiUtils {
     public static <T extends TameableEntity & Shikigami> void toggleShikigami(PlayerEntity player, T shikigami) {
         if (player.getWorld().isClient || !(player.getWorld() instanceof ServerWorld serverWorld)) return;
 
-        Map<String, NbtCompound> map = HeroUtils.getHeroStack(player).getOrDefault(ShadowHero.SHIKIGAMI, new HashMap<>());
+        ItemStack heroStack = HeroUtils.getHeroStack(player);
+        Map<String, NbtCompound> map = heroStack.getOrDefault(ShadowHero.SHIKIGAMI, new HashMap<>());
         HashMap<String, NbtCompound> clone = new HashMap<>(map);
 
-        NbtCompound nbt = map.getOrDefault(shikigami.getType().toString(), new NbtCompound());
+        String shikigamiKey = shikigami.getType().toString();
+        NbtCompound nbt = map.getOrDefault(shikigamiKey, new NbtCompound());
 
-        if (!nbt.getBoolean("summoned")) {
+        boolean nowSummoned = !nbt.getBoolean("summoned");
+        nbt.putBoolean("summoned", nowSummoned);
+
+        if (nowSummoned) {
+            nbt.remove("UUID");
+
             Optional<Entity> entityOptional = EntityType.getEntityFromNbt(nbt, serverWorld);
+            T newEntity = entityOptional.isPresent() ? summonShikigami(player, (T) entityOptional.get()) : summonShikigami(player, shikigami);
 
-            if (entityOptional.isPresent()) {
-                summonShikigami(player, (T) entityOptional.get());
-            } else {
-                T newEntity = summonShikigami(player, shikigami);
-                newEntity.writeNbt(nbt);
-            }
+            newEntity.setPersistent();
+            newEntity.setTamed(true, false);
+            newEntity.saveNbt(nbt);
         } else {
             UUID uuid = nbt.getUuid("UUID");
-            if (serverWorld.getEntity(uuid) != null) {
-                Entity entity = serverWorld.getEntity(uuid);
-                if (entity != null) {
-                    entity.writeNbt(nbt);
-                    entity.discard();
-                }
+            Entity entity = serverWorld.getEntity(uuid);
+            if (entity != null) {
+                entity.saveNbt(nbt);
+                entity.discard();
             }
         }
 
-        nbt.putBoolean("summoned", !nbt.getBoolean("summoned"));
-
-        clone.put(shikigami.getType().toString(), nbt);
-        HeroUtils.getHeroStack(player).set(ShadowHero.SHIKIGAMI, clone);
+        clone.put(shikigamiKey, nbt);
+        heroStack.set(ShadowHero.SHIKIGAMI, clone);
     }
 
     public static <T extends TameableEntity & Shikigami> T summonShikigami(PlayerEntity player, T shikigami) {
-        shikigami.setPos(player.getPos().getX(), player.getPos().getY(), player.getPos().getZ());
+        shikigami.setPos(player.getX(), player.getY(), player.getZ());
         shikigami.setOwner(player);
         player.getWorld().spawnEntity(shikigami);
         return shikigami;
     }
-
-    /*
-    // Todo: extracting this out to a generic summon method soon
-    public static void summonDivineDogKuro(PlayerEntity player, DivineDogKuroEntity shikigami) {
-        if (player.getWorld().isClient) return;
-        Map<String, NbtCompound> shikigamiMap = HeroUtils.getHeroStack(player).getOrDefault(ShadowHero.SHIKIGAMI, new HashMap<>());
-        if (shikigamiMap.get(shikigami.getName().getString()) == null) {
-            DivineDogKuroEntity entity = summonOnTopBlock(player, shikigami);
-            if (entity != null) {
-                NbtCompound data = new NbtCompound();
-
-                entity.writeCustomDataToNbt(data);
-                data.putUuid("uuid", entity.getUuid());
-
-                shikigamiMap.put(shikigami.getName().getString(), data);
-                HeroUtils.getHeroStack(player).set(ShadowHero.SHIKIGAMI, shikigamiMap);
-                System.out.println("Summoned new shikigami");
-            }
-        } else {
-            System.out.println(shikigamiMap.get(shikigami.getName().getString()).getUuid("uuid"));
-            System.out.println("Desummoned shikigami");
-        }
-    }
-
-    public static DivineDogKuroEntity summonOnTopBlock(PlayerEntity player, DivineDogKuroEntity shikigami) {
-        BlockHitResult blockHitResult = RaycastUtils.blockRaycast(player, 16);
-        if (blockHitResult == null) return null;
-
-        BlockPos pos = blockHitResult.getBlockPos();
-
-        for (int i = 0; i < 64; i++) {
-            if (player.getWorld().getBlockState(pos.up(i)).isAir()) {
-                pos = pos.up();
-                break;
-            }
-        }
-
-        shikigami.setPos(pos.getX(), pos.getY(), pos.getZ());
-        shikigami.setOwner(player);
-        player.getWorld().spawnEntity(shikigami);
-        return shikigami;
-    }
-
-     */
 }
