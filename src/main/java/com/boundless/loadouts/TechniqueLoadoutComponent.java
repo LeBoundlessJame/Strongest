8b5bd14a0583a794e4c9cae62667a00d479c76be
j <j@noreply.com>
2026-08-24 18:48:03 +0100

    Updated TechniqueLoadoutComponent to use correct CODEC

--- src/main/java/com/boundless/loadouts/TechniqueLoadoutComponent.java
+++ src/main/java/com/boundless/loadouts/TechniqueLoadoutComponent.java
@@ -1,7 +1,12 @@
 package com.boundless.loadouts;
 
+
+import com.mojang.serialization.Codec;
 import net.minecraft.util.Identifier;
 
 import java.util.Map;
 
-public record TechniqueLoadoutComponent(Map<AbilityKey, Identifier> abilities) {}
+public record TechniqueLoadoutComponent(Map<AbilityKey, Identifier> abilities) {
+    public static final Codec<TechniqueLoadoutComponent> CODEC = Codec.unboundedMap(AbilityKey.CODEC, Identifier.CODEC)
+            .xmap(TechniqueLoadoutComponent::new, TechniqueLoadoutComponent::abilities);
+}
