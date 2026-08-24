97150d5b26dd3813d7e2940971884ac06ba6b4ac
j <j@noreply.com>
2026-08-24 18:33:37 +0100

    Updated AbilityKey to use codec

--- src/main/java/com/boundless/loadouts/AbilityKey.java
+++ src/main/java/com/boundless/loadouts/AbilityKey.java
@@ -1,8 +1,11 @@
 package com.boundless.loadouts;
 
+import com.mojang.serialization.Codec;
 import lombok.Getter;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.StringIdentifiable;
 
-public enum AbilityKey {
+public enum AbilityKey implements StringIdentifiable {
     ATTACK("key.attack"),
     USE("key.use"),
     ABILITY_ONE("key.boundless.ability_one"),
@@ -10,10 +13,17 @@ public enum AbilityKey {
     ABILITY_THREE("key.boundless.ability_three"),
     ABILITY_FOUR("key.boundless.ability_four");
 
+    public static final Codec<AbilityKey> CODEC = StringIdentifiable.createBasicCodec(AbilityKey::values);
+
     AbilityKey(String translationKey) {
         this.translationKey = translationKey;
     }
 
     @Getter
     private final String translationKey;
+
+    @Override
+    public String asString() {
+        return this.translationKey;
+    }
 }
