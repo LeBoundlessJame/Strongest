f9a0e753bc3074a72ba3927e85ddd34a31c1b55e
j <j@noreply.com>
2026-08-25 20:39:04 +0100

    Added hotbar left and hotbar right for offhand items (thanks again foily)

--- src/main/java/com/boundless/client/StatOverlays.java
+++ src/main/java/com/boundless/client/StatOverlays.java
@@ -14,6 +14,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Arm;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
 import org.joml.Math;
@@ -23,6 +24,8 @@ import java.util.Map;
 
 public class StatOverlays {
     public static final Identifier HOTBAR = BoundlessAPI.hudPNG("hotbar");
+    public static final Identifier HOTBAR_LEFT = BoundlessAPI.hudPNG("hotbar_left");
+    public static final Identifier HOTBAR_RIGHT = BoundlessAPI.hudPNG("hotbar_right");
     public static final Identifier HEALTH = BoundlessAPI.hudPNG("health");
     public static final Identifier CURSED_ENERGY = BoundlessAPI.hudPNG("cursed_energy");
 
@@ -38,10 +41,17 @@ public class StatOverlays {
         context.drawTexture(HEALTH, x, y, 0, 0, 0, healthProgress, 10, 66, 10);
     }
 
-    public static void renderHotbar(DrawContext context) {
+    public static void renderHotbar(MinecraftClient client, DrawContext context) {
         int x = context.getScaledWindowWidth() / 2 - 130;
         int y = context.getScaledWindowHeight() - 40;
-        context.drawTexture(HOTBAR, x, y, 0, 0, 260, 40, 260, 40);
+
+        Identifier hotbar = HOTBAR;
+
+        if (!client.player.getOffHandStack().isEmpty()) {
+            hotbar = client.player.getMainArm() == Arm.LEFT ? HOTBAR_LEFT : HOTBAR_RIGHT;
+        }
+
+        context.drawTexture(hotbar, x, y, 0, 0, 260, 40, 260, 40);
     }
 
     public static void renderHealthText(MinecraftClient client, DrawContext context) {
