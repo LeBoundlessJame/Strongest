229bf6f22dcb57715b2f85910ffe72a9edbc1a91
j <j@noreply.com>
2026-08-26 17:47:48 +0100

    Added some helpful methods to CursedEnergyManager

--- src/main/java/com/boundless/mechanics/CursedEnergyManager.java
+++ src/main/java/com/boundless/mechanics/CursedEnergyManager.java
@@ -1,5 +1,30 @@
 package com.boundless.mechanics;
 
-public class CursedEnergyManager {
+import com.boundless.registry.DataComponentRegistry;
+import com.boundless.util.DataComponentUtils;
+import com.boundless.util.HeroUtils;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 
+import java.util.Map;
+
+public class CursedEnergyManager {
+    public static void setCursedEnergy(PlayerEntity player, int cursedEnergy) {
+        int clampedCE = MathHelper.clamp(cursedEnergy, 0, getMaxCursedEnergy(player));
+        HeroUtils.getHeroStack(player).set(DataComponentRegistry.CURSED_ENERGY, clampedCE);
+    }
+
+    public static void addCursedEnergy(PlayerEntity player, int cursedEnergy) {
+        setCursedEnergy(player, getCursedEnergy(player) + cursedEnergy);
+    }
+
+    public static int getMaxCursedEnergy(PlayerEntity player) {
+        return HeroUtils.getHeroStack(player).getOrDefault(DataComponentRegistry.CURSED_ENERGY_MAX, 0);
+    }
+
+    public static int getCursedEnergy(PlayerEntity player) {
+        return HeroUtils.getHeroStack(player).getOrDefault(DataComponentRegistry.CURSED_ENERGY, getMaxCursedEnergy(player));
+    }
 }
--- src/main/java/com/boundless/registry/DataComponentRegistry.java
+++ src/main/java/com/boundless/registry/DataComponentRegistry.java
@@ -15,6 +15,10 @@ import java.util.function.UnaryOperator;
 
 // Todo: Clean this up!!
 public class DataComponentRegistry {
+    public static ComponentType<TechniqueLoadoutComponent> TECHNIQUE_LOADOUT = registerComponent("technique_loadout", builder -> ComponentType.<TechniqueLoadoutComponent>builder().codec(TechniqueLoadoutComponent.CODEC));
+    public static ComponentType<Integer> CURSED_ENERGY = registerComponent("cursed_energy", builder -> ComponentType.<Integer>builder().codec(Codec.INT));
+    public static ComponentType<Integer> CURSED_ENERGY_MAX = registerComponent("cursed_energy_max", builder -> ComponentType.<Integer>builder().codec(Codec.INT));
+
     public static final ComponentType<Map<String, Identifier>> ABILITY_LOADOUT = registerComponent("ability_loadout", builder -> ComponentType.<Map<String, Identifier>>builder().codec(Codec.unboundedMap(Codec.STRING, Identifier.CODEC)));
     public static final ComponentType<Map<Identifier, Long>> COOLDOWN_DATA = registerComponent("cooldown_data", builder -> ComponentType.<Map<Identifier, Long>>builder().codec(Codec.unboundedMap(Identifier.CODEC, Codec.LONG)));
     public static ComponentType<Long> ATTACK_START = registerComponent("attack_start", builder -> ComponentType.<Long>builder().codec(Codec.LONG));
@@ -23,9 +27,6 @@ public class DataComponentRegistry {
     public static ComponentType<Integer> SPRINT_TICKS = registerComponent("sprint_ticks", builder -> ComponentType.<Integer>builder().codec(Codec.INT));
     public static ComponentType<Integer> ATTACK_COUNT = registerComponent("attack_count", builder -> ComponentType.<Integer>builder().codec(Codec.INT));
     public static ComponentType<Boolean> COMBAT_MODE_ENABLED = DataComponentRegistry.registerComponent("combat_mode_enabled", builder -> ComponentType.<Boolean>builder().codec(Codec.BOOL));
-
-    public static ComponentType<TechniqueLoadoutComponent> TECHNIQUE_LOADOUT = registerComponent("technique_loadout", builder -> ComponentType.<TechniqueLoadoutComponent>builder().codec(TechniqueLoadoutComponent.CODEC));
-
     public static final ComponentType<Map<String, KeybindHoldData>> HELD_KEYBIND = registerComponent("held_keybind", builder -> ComponentType.<Map<String, KeybindHoldData>>builder().codec(Codec.unboundedMap(Codec.STRING, CodecRegistry.KEYBIND_HOLD_CODEC)));
 
     public static <T> ComponentType<T> registerComponent(String name, UnaryOperator<ComponentType.Builder<T>> builder) {
