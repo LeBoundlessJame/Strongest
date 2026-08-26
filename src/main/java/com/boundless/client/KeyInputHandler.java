851712d900c9a1cc3abd856e8cedd3fd5011ee8d
j <j@noreply.com>
2026-08-26 13:03:42 +0100

    Added fix for keybind conflicts, now you can use abilities even if there's conflicts

--- src/main/java/com/boundless/client/KeyInputHandler.java
+++ src/main/java/com/boundless/client/KeyInputHandler.java
@@ -10,12 +10,14 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 public class KeyInputHandler {
     public static void keyInputs() {
         ClientTickEvents.START_CLIENT_TICK.register(client -> {
-            if (client.player == null || !HeroUtils.isHero(client.player)) return;
+            if (client.player == null || client.currentScreen != null || !HeroUtils.isHero(client.player)) return;
 
             TechniqueLoadout loadout = HeroUtils.getTechniqueLoadout(client.player);
 
@@ -29,7 +31,7 @@ public class KeyInputHandler {
 
     private static void inputLogic(MinecraftClient client, AbilityKey abilityKey, TechniqueLoadout loadout) {
         KeyBinding key = KeybindingUtils.getKeyBindingFromTranslation(abilityKey.getTranslationKey());
-        if (!key.isPressed()) return;
+        if (key == null || !isKeybindingPressed(key)) return;
 
         Identifier abilityID = loadout.getAbilityId(abilityKey, client.player);
 
@@ -38,5 +40,18 @@ public class KeyInputHandler {
         }
 
         ClientPlayNetworking.send(new AbilityUsePayload(abilityID));
+    }
+
+    private static boolean isKeybindingPressed(KeyBinding keyBinding) {
+        InputUtil.Key key = InputUtil.fromTranslationKey(keyBinding.getBoundKeyTranslationKey());
+        long handle = MinecraftClient.getInstance().getWindow().getHandle();
+
+        if (key.getCategory() == InputUtil.Type.KEYSYM) {
+            return InputUtil.isKeyPressed(handle, key.getCode());
+        } else if (key.getCategory() == InputUtil.Type.MOUSE) {
+            return GLFW.glfwGetMouseButton(handle, key.getCode()) == GLFW.GLFW_PRESS;
+        }
+
+        return false;
     }
 }
