f5f4ff4fec71a636b9fa06569b35113e43db1ecd
j <j@noreply.com>
2026-08-27 12:57:50 +0100

    Put gama grapple into a dedicated ability

--- src/main/java/com/boundless/hero/shadow_hero/technique/abilities/GamaGrappleAbility.java
+++ src/main/java/com/boundless/hero/shadow_hero/technique/abilities/GamaGrappleAbility.java
@@ -2,14 +2,37 @@ package com.boundless.hero.shadow_hero.technique.abilities;
 
 import com.boundless.BoundlessAPI;
 import com.boundless.ability.TechniqueAbility;
-import com.boundless.entity.gama.abilities.GamaGrapple;
+import com.boundless.entity.gama.GamaEntity;
+import com.boundless.entity.grapple.GrappleEntity;
+import com.boundless.util.RaycastUtils;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.Vec3d;
 
 public class GamaGrappleAbility extends TechniqueAbility {
     @Override
     public void activate(PlayerEntity player) {
-        GamaGrapple.grappleLogic(player);
+        if (player.getWorld().isClient) return;
+        if (!(player.getControllingVehicle() instanceof GamaEntity gama)) return;
+
+        GrappleEntity grapple = gama.getGrapple();
+
+        if (grapple == null) {
+            BlockHitResult blockHitResult = RaycastUtils.blockRaycast(player, 128);
+            if (blockHitResult == null) return;
+
+            grapple = new GrappleEntity(gama, player.getWorld());
+            grapple.setVelocity(0, 0, 0);
+            grapple.setPosition(Vec3d.of(blockHitResult.getBlockPos()));
+            grapple.setNoGravity(true);
+            player.getWorld().spawnEntity(grapple);
+
+            gama.setGrapple(grapple);
+        } else {
+            grapple.discard();
+            gama.setGrapple(null);
+        }
     }
 
     @Override
@@ -20,5 +43,11 @@ public class GamaGrappleAbility extends TechniqueAbility {
     @Override
     public long getCooldown() {
         return 2;
+    }
+
+    @Override
+    public boolean canActivate(PlayerEntity player) {
+        return super.canActivate(player) &&
+                player.getControllingVehicle() instanceof GamaEntity gama && gama.getOwner() == player;
     }
 }
