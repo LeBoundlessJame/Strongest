7d7de45d118ea795bf8984cc24901fa49e9927ce
j <j@noreply.com>
2026-08-27 15:10:54 +0100

    Bound left click back to punch

--- src/main/java/com/boundless/hero/shadow_hero/ShadowHero.java
+++ src/main/java/com/boundless/hero/shadow_hero/ShadowHero.java
@@ -33,7 +33,7 @@ public class ShadowHero extends Hero {
 
     public ShadowHero() {
         TechniqueLoadout loadout = TechniqueLoadout.builder()
-                .ability(AbilityKey.ATTACK, TenShadowsTechnique.ROUNDHOUSE_KICK)
+                .ability(AbilityKey.ATTACK, TenShadowsTechnique.PUNCH)
                 .ability(AbilityKey.USE, TenShadowsTechnique::getRightClickAbility)
                 .ability(AbilityKey.ABILITY_TWO, TenShadowsTechnique.SHIRO)
                 .ability(AbilityKey.ABILITY_THREE, TenShadowsTechnique.GAMA)
