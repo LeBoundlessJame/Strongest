00be571f5532671f4389c639c39680c491bcd88c
j <j@noreply.com>
2026-08-28 15:10:40 +0100

    WIP gama entity renderer (rendering in wrong direction, working on a fix)

--- src/main/java/com/boundless/entity/gama/GamaEntityRenderer.java
+++ src/main/java/com/boundless/entity/gama/GamaEntityRenderer.java
@@ -1,17 +1,46 @@
 package com.boundless.entity.gama;
 
 import com.boundless.BoundlessAPI;
+import com.boundless.entity.grapple.GrappleEntityRenderer;
 import mod.azure.azurelib.common.render.entity.AzEntityRenderer;
 import mod.azure.azurelib.common.render.entity.AzEntityRendererConfig;
+import net.minecraft.client.render.Frustum;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
 
-public class GamaEntityRenderer<T extends AnimalEntity> extends AzEntityRenderer<T> {
+public class GamaEntityRenderer<T extends GamaEntity> extends AzEntityRenderer<T> {
     private static final Identifier GEO = BoundlessAPI.id("geo/entity/gama.geo.json");
     private static final Identifier TEXTURE = BoundlessAPI.id("textures/entity/gama.png");
 
     public GamaEntityRenderer(EntityRendererFactory.Context context) {
         super(AzEntityRendererConfig.<T>builder(GEO, TEXTURE).setAnimatorProvider(GamaAnimator::new).build(), context);
+    }
+
+    @Override
+    public void render(
+            @NotNull T gama,
+            float entityYaw,
+            float partialTick,
+            @NotNull MatrixStack poseStack,
+            @NotNull VertexConsumerProvider bufferSource,
+            int packedLight
+    ) {
+        super.render(gama, entityYaw, partialTick, poseStack, bufferSource, packedLight);
+
+        if (gama.getPullTimer() <= 0) return;
+        Entity target = gama.getPullTarget();
+
+        if (target == null) return;
+        GrappleEntityRenderer.renderRope(target, partialTick, poseStack, bufferSource, gama);
+    }
+
+    @Override
+    public boolean shouldRender(T entity, Frustum frustum, double x, double y, double z) {
+        return true;
     }
 }
