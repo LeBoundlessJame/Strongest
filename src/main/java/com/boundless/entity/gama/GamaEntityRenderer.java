eaf22f10485705699fb195671359952992c027e6
j <j@noreply.com>
2026-08-28 15:54:25 +0100

    Gama conditional bone rendering in

--- src/main/java/com/boundless/entity/gama/GamaEntityRenderer.java
+++ src/main/java/com/boundless/entity/gama/GamaEntityRenderer.java
@@ -3,35 +3,49 @@ package com.boundless.entity.gama;
 import com.boundless.BoundlessAPI;
 import com.boundless.entity.grapple.GrappleEntityRenderer;
 import com.boundless.util.RenderUtils;
+import mod.azure.azurelib.common.model.AzBone;
+import mod.azure.azurelib.common.render.AzRendererPipelineContext;
 import mod.azure.azurelib.common.render.entity.AzEntityRenderer;
 import mod.azure.azurelib.common.render.entity.AzEntityRendererConfig;
+import mod.azure.azurelib.common.render.entity.AzEntityRendererPipelineContext;
 import net.minecraft.client.render.Frustum;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class GamaEntityRenderer<T extends GamaEntity> extends AzEntityRenderer<T> {
     private static final Identifier GEO = BoundlessAPI.id("geo/entity/gama.geo.json");
     private static final Identifier TEXTURE = BoundlessAPI.id("textures/entity/gama.png");
 
     public GamaEntityRenderer(EntityRendererFactory.Context context) {
-        super(AzEntityRendererConfig.<T>builder(GEO, TEXTURE).setAnimatorProvider(GamaAnimator::new).build(), context);
+        super(AzEntityRendererConfig.<T>builder(GEO, TEXTURE).setAnimatorProvider(GamaAnimator::new).setPrerenderEntry(pipelineContext -> {
+            T gama = pipelineContext.animatable();
+
+            boolean isTheBoss = gama.hasCustomName() && gama.getCustomName().equals(Text.literal("Large employer"));
+
+            pipelineContext.bakedModel().getBone("big_boss").ifPresent(bone -> {
+                bone.setHidden(!isTheBoss);
+            });
+
+            pipelineContext.bakedModel().getBone("big_boss2").ifPresent(bone -> {
+                bone.setHidden(!isTheBoss);
+            });
+
+            return pipelineContext;
+        }).build(), context);
     }
 
     @Override
-    public void render(
-            @NotNull T gama,
-            float entityYaw,
-            float partialTick,
-            @NotNull MatrixStack poseStack,
-            @NotNull VertexConsumerProvider bufferSource,
-            int packedLight
-    ) {
+    public void render(@NotNull T gama, float entityYaw, float partialTick, @NotNull MatrixStack poseStack, @NotNull VertexConsumerProvider bufferSource, int packedLight) {
         super.render(gama, entityYaw, partialTick, poseStack, bufferSource, packedLight);
 
         if (gama.getPullTimer() <= 0) return;
