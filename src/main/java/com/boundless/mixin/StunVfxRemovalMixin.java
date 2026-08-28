296066a146b46af688649d1b17c8704f0dbfe38b
j <j@noreply.com>
2026-08-29 00:11:45 +0100

    Stun removal mixin working

---
+++ src/main/java/com/boundless/mixin/StunVfxRemovalMixin.java
@@ -0,0 +1,21 @@
+package com.boundless.mixin;
+
+import com.boundless.BoundlessAPI;
+import com.boundless.effect.StunEffect;
+import com.boundless.util.VFXUtils;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+public class StunVfxRemovalMixin {
+    @Inject(at = @At("HEAD"), method = "onStatusEffectRemoved")
+    protected void boundless$onStatusEffectRemoved(StatusEffectInstance effect, CallbackInfo ci) {
+        if (effect.getEffectType().value() instanceof StunEffect) {
+            VFXUtils.destroyEffectInstance("stun", ((LivingEntity)(Object)this).getId());
+        }
+    }
+}
