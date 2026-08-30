adc68899c0b3fd734760489e150bdf173bb82b94
j <j@noreply.com>
2026-08-30 11:44:54 +0100

    playBlackFlashVisuals added

--- src/main/java/com/boundless/mechanics/BlackFlashManager.java
+++ src/main/java/com/boundless/mechanics/BlackFlashManager.java
@@ -1,7 +1,10 @@
 package com.boundless.mechanics;
 
+import com.boundless.registry.StatusEffectRegistry;
 import com.boundless.registry.StrongestComponents;
 import com.boundless.util.HeroUtils;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
 
 public class BlackFlashManager {
@@ -22,7 +25,8 @@ public class BlackFlashManager {
         return HeroUtils.getHeroStack(player).getOrDefault(StrongestComponents.BLACK_FLASH_DAMAGE_MULTIPLIER, 2.5f);
     }
 
-    public static void blackFlashImpact(PlayerEntity player) {
-
+    public static void playBlackFlashVisuals(LivingEntity entity, int duration) {
+        entity.addStatusEffect(new StatusEffectInstance(StatusEffectRegistry.IMPACT_FRAME_EFFECT, duration - 2, 4, true, false, false));
+        entity.addStatusEffect(new StatusEffectInstance(StatusEffectRegistry.CLAP_IMPACT_FRAME_EFFECT, duration, 4, true, false, false));
     }
 }
