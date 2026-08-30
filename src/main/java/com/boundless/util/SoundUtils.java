c696b4736e16202232d29fb139d986a9c1233400
j <j@noreply.com>
2026-08-30 14:27:45 +0100

    Updated SoundUtils to allow entity

--- src/main/java/com/boundless/util/SoundUtils.java
+++ src/main/java/com/boundless/util/SoundUtils.java
@@ -1,24 +1,25 @@
 package com.boundless.util;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 
 public class SoundUtils {
 
-    public static void playSound(PlayerEntity player, SoundEvent sound, float pitch) {
-        if (!player.getWorld().isClient) {
-            player.getWorld().playSound(null, player.getX(), player.getY(), player.getZ(), sound, SoundCategory.PLAYERS, 1.0f, pitch);
+    public static void playSound(LivingEntity entity, SoundEvent sound, float pitch) {
+        if (!entity.getWorld().isClient) {
+            entity.getWorld().playSound(null, entity.getX(), entity.getY(), entity.getZ(), sound, SoundCategory.PLAYERS, 1.0f, pitch);
         }
     }
 
-    public static void playSound(PlayerEntity player, SoundEvent sound) {
-        float pitch = player.getRandom().nextBetween(6, 15) * 0.1f;
-        playSound(player, sound, pitch);
+    public static void playSound(LivingEntity entity, SoundEvent sound) {
+        float pitch = entity.getRandom().nextBetween(6, 15) * 0.1f;
+        playSound(entity, sound, pitch);
     }
 
-    public static void playSound(PlayerEntity player, SoundEvent sound, int lowerRange, int upperRange) {
-        float pitch = player.getRandom().nextBetween(lowerRange, upperRange) * 0.1f;
-        playSound(player, sound, pitch);
+    public static void playSound(LivingEntity entity, SoundEvent sound, int lowerRange, int upperRange) {
+        float pitch = entity.getRandom().nextBetween(lowerRange, upperRange) * 0.1f;
+        playSound(entity, sound, pitch);
     }
 }
