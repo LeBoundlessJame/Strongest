1c23c8ee293507c234f21f579c674cd1acfd1ee0
j <j@noreply.com>
2026-08-30 14:06:52 +0100

    Removed performAttack

--- src/main/java/com/boundless/util/AttackUtils.java
+++ src/main/java/com/boundless/util/AttackUtils.java
@@ -1,51 +1,20 @@
 package com.boundless.util;
 
 import com.boundless.action.Action;
-import com.boundless.action.SingleAttack;
 import com.boundless.entity.hero_action.HeroActionEntity;
 import com.boundless.registry.DataComponentRegistry;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
 import java.util.LinkedHashMap;
-import java.util.Optional;
 import java.util.function.BiConsumer;
 
 import static com.boundless.registry.DataComponentRegistry.ATTACK_END;
 import static com.boundless.registry.DataComponentRegistry.ATTACK_START;
 
 public class AttackUtils {
-    public static void performAttack(SingleAttack singleAttack) {
-        PlayerEntity player = singleAttack.getPlayer();
-        if (!AttackUtils.canAttack(player)) return;
-
-        float damage = singleAttack.getDamage();
-        SoundEvent sound = singleAttack.getImpactSound();
-        Identifier animation = singleAttack.getAnimation();
-        Identifier attackVFX = singleAttack.getImpactVFX();
-        float animationSpeed = singleAttack.getAnimationSpeed();
-        int priority = singleAttack.getAnimationPriority();
-        int attackDuration = singleAttack.getAttackDuration();
-        int impactTick = singleAttack.getImpactTick();
-
-        DataComponentUtils.incrementInt(DataComponentRegistry.ATTACK_COUNT, player, 1);
-        int attackCount = HeroUtils.getHeroStack(player).getOrDefault(DataComponentRegistry.ATTACK_COUNT, 0);
-
-        LinkedHashMap<Integer, BiConsumer<PlayerEntity, HeroActionEntity>> tasks = new LinkedHashMap<>();
-        BiConsumer<PlayerEntity, HeroActionEntity> hit = (user, heroAction) -> {
-            if (CombatUtils.isRolling(player)) return;
-            SoundUtils.playSound(player, sound);
-            CombatUtils.hit(player, heroAction, damage);
-        };
-        tasks.put(impactTick, hit);
-        PlayerAnimationUtils.playSyncedAnimation(player, animation, animationSpeed, attackCount % 2 == 0, true, priority);
-        ActionUtils.performAction(player, Action.builder().scheduledTasks(tasks).build());
-        AttackUtils.startAttackTimer(player, attackDuration);
-    }
-
     public static void startAttackTimer(PlayerEntity player, long duration) {
         ItemStack heroStack = HeroUtils.getHeroStack(player);
         heroStack.set(ATTACK_START, player.getWorld().getTime());
