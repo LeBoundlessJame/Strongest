52923e42c125a8bc226e80fc55e34bd85c9b944f
j <j@noreply.com>
2026-08-31 14:38:11 +0100

    Using the proper black flash effect now, testing with 100% black flash chance

--- src/main/java/com/boundless/hero/ratio_technique_hero/RatioHero.java
+++ src/main/java/com/boundless/hero/ratio_technique_hero/RatioHero.java
@@ -32,7 +32,7 @@ public class RatioHero extends Hero {
                 .tickHandler(Hero::onHeroTick)
                 .tickHandler(RatioTechnique::ratioTick)
                 .maxCursedEnergy(7000)
-                .blackFlashChance(0.02f)
+                .blackFlashChance(1)
                 .blackFlashDamageMultiplier(2.5f)
                 .attackModifier(new BlackFlashModifier())
                 .attackModifier(new RatioModifier())
--- src/main/java/com/boundless/hero/ratio_technique_hero/technique/abilities/CollapseAbility.java
+++ src/main/java/com/boundless/hero/ratio_technique_hero/technique/abilities/CollapseAbility.java
@@ -4,19 +4,32 @@ import com.boundless.BoundlessAPI;
 import com.boundless.ability.TechniqueAbility;
 import com.boundless.ability.generic.AOEAbility;
 import com.boundless.ability.generic.PunchAbility;
+import com.boundless.entity.hero_action.HeroActionEntity;
+import com.boundless.util.AOE;
+import com.boundless.util.AOEUtils;
+import com.boundless.util.CombatUtils;
+import com.boundless.util.EffekUtils;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
 
+import java.util.List;
+
 @Getter @Setter @Accessors(chain = true)
-public class CollapseAbility extends AOEAbility {
+public class CollapseAbility extends AOEAbility implements AOE {
     @Override
     public Identifier getAbilityId() {
         return BoundlessAPI.id("collapse");
+    }
+
+    public void activateAOE(PlayerEntity player, HeroActionEntity action) {
+        super.activateAOE(player, action);
+        EffekUtils.playVisual(player, BoundlessAPI.id(""));
     }
 
     @Override
