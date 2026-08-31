971b339e46c971965c8b6f986902fd2fd4932e93
j <j@noreply.com>
2026-08-31 17:43:31 +0100

    Added 7% of enemy's max health to hit damage

--- src/main/java/com/boundless/combat/attack_modifiers/RatioModifier.java
+++ src/main/java/com/boundless/combat/attack_modifiers/RatioModifier.java
@@ -22,8 +22,10 @@ public class RatioModifier implements AttackModifier {
     @Override
     public void apply(Hit hit) {
         hit.multiplyDamage(1.75f);
+        hit.addDamage(hit.getTarget().getMaxHealth() * 0.07f);
         hit.getHitEffects().addSounds(List.of(SoundRegistry.ENERGY_IMPACT_2, SoundRegistry.ENERGY_IMPACT_HEAVY));
         hit.getHitEffects().addVisual(BoundlessAPI.id("ratio_impact"));
+        hit.getHitEffects().addVisual(BoundlessAPI.id("slash"));
     }
 
     @Override
