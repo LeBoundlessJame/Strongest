1469b8792939786cb93303ce420db11d3e2c4b70
j <j@noreply.com>
2026-09-01 16:50:57 +0100

    Ratio skillcheck now uses cleaver-non-cleaver logic

--- src/main/java/com/boundless/hero/ratio_technique_hero/technique/abilities/RatioAbility.java
+++ src/main/java/com/boundless/hero/ratio_technique_hero/technique/abilities/RatioAbility.java
@@ -5,8 +5,10 @@ import com.boundless.ability.TechniqueAbility;
 import com.boundless.hero.ratio_technique_hero.technique.RatioComponents;
 import com.boundless.hero.ratio_technique_hero.technique.RatioSkillcheck;
 import com.boundless.mechanics.CooldownManager;
+import com.boundless.registry.ItemRegistry;
 import com.boundless.util.HeroUtils;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
@@ -14,18 +16,24 @@ public class RatioAbility extends TechniqueAbility {
     @Override
     public void activate(PlayerEntity player) {
         if (player.getWorld().isClient) return;
-        RatioSkillcheck skillcheck = HeroUtils.getHeroStack(player).get(RatioComponents.RATIO_SKILLCHECK);
+
+        ItemStack stack = HeroUtils.getHeroStack(player);
+        RatioSkillcheck skillcheck = stack.get(RatioComponents.RATIO_SKILLCHECK);
 
         if (skillcheck == null) {
-            RatioAbility.startSkillcheck(player, 40, 20, 2);
-        } else {
-            if (skillcheck.isSuccessful(player.getWorld().getTime())) {
-                HeroUtils.getHeroStack(player).set(RatioComponents.RATIO_NEXT_ATTACK, true);
-            }
+            long leniency = isUsingCleaver(player) ? 3 : 1;
+            float damageMultiplierReward = isUsingCleaver(player) ? 1.75f : 2.0f;
 
-            HeroUtils.getHeroStack(player).remove(RatioComponents.RATIO_SKILLCHECK);
-            CooldownManager.setAbilityCooldownIfHigher(player, this.getAbilityId(), 200);
+            startSkillcheck(player, 40, 20, leniency, damageMultiplierReward);
+            return;
         }
+
+        if (skillcheck.isSuccessful(player.getWorld().getTime())) {
+            stack.set(RatioComponents.NEXT_ATTACK_RATIO_MULTIPLIER, skillcheck.nextAttackRatioMultiplier());
+        }
+
+        stack.remove(RatioComponents.RATIO_SKILLCHECK);
+        CooldownManager.setAbilityCooldownIfHigher(player, this.getAbilityId(), 200);
     }
 
     @Override
@@ -48,10 +56,14 @@ public class RatioAbility extends TechniqueAbility {
         return Text.of("Ratio");
     }
 
-    private static void startSkillcheck(PlayerEntity player, long duration, long ticksUntilTarget, long leniency) {
+    private static void startSkillcheck(PlayerEntity player, long duration, long ticksUntilTarget, long leniency, float damageMultiplierReward) {
         long startTick = player.getWorld().getTime();
 
-        RatioSkillcheck skillcheck = new RatioSkillcheck(startTick, startTick + duration, startTick + ticksUntilTarget, leniency);
+        RatioSkillcheck skillcheck = new RatioSkillcheck(startTick, startTick + duration, startTick + ticksUntilTarget, leniency, damageMultiplierReward);
         HeroUtils.getHeroStack(player).set(RatioComponents.RATIO_SKILLCHECK, skillcheck);
+    }
+
+    private static boolean isUsingCleaver(PlayerEntity player) {
+        return player.getMainHandStack().isOf(ItemRegistry.CLEAVER);
     }
 }
