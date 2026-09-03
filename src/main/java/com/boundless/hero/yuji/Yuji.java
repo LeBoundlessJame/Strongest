f94fd17a6d8f134881a1d86c86d26e810ad9c15e
j <j@noreply.com>
2026-09-03 23:17:51 +0100

    Nerfed yuji black flash multiplier

--- src/main/java/com/boundless/hero/yuji/Yuji.java
+++ src/main/java/com/boundless/hero/yuji/Yuji.java
@@ -4,11 +4,10 @@ import com.boundless.BoundlessAPI;
 import com.boundless.combat.attack_modifiers.BlackFlashModifier;
 import com.boundless.hero.api.Hero;
 import com.boundless.hero.api.HeroData;
-import com.boundless.hero.nanami.technique.RatioAttributes;
 import com.boundless.hero.nanami.technique.RatioTechnique;
 import com.boundless.hero.nanami.technique.RatioTechniqueAbilityResolver;
 import com.boundless.hero.yuji.technique.YujiAttributes;
-import com.boundless.hero.yuji.technique.YujiComponents;
+import com.boundless.hero.yuji.technique.components.YujiComponents;
 import com.boundless.hero.yuji.technique.YujiTechnique;
 import com.boundless.loadouts.AbilityKey;
 import com.boundless.loadouts.TechniqueLoadout;
@@ -20,9 +19,9 @@ public class Yuji extends Hero {
         // Todo: Might make roundhouse generic, and then make it calculated on player's melee stat
         // For now, will use megumi's roundhouse until I get things setup
         TechniqueLoadout loadout = TechniqueLoadout.builder()
-                .ability(AbilityKey.ATTACK, RatioTechnique.PUNCH)
+                .ability(AbilityKey.ATTACK, YujiTechnique.PUNCH)
                 .ability(AbilityKey.ABILITY_ONE, YujiTechnique.DIVERGENT_ENERGY)
-                .ability(AbilityKey.USE, RatioTechniqueAbilityResolver::getRightClickAbility).build();
+                .ability(AbilityKey.USE, YujiTechnique.ROUNDHOUSE_KICK).build();
 
         this.heroData = HeroData.builder()
                 .name("yuji")
@@ -34,7 +33,7 @@ public class Yuji extends Hero {
                 .tickHandler(Hero::onHeroTick)
                 .maxCursedEnergy(5000)
                 .blackFlashChance(0.08f)
-                .blackFlashDamageMultiplier(2.5f)
+                .blackFlashDamageMultiplier(1.9f)
                 .attackModifier(new BlackFlashModifier())
                 .build();
 
