ac7a65a1386495bb63a0da1e28b299529ff25e02
j <j@noreply.com>
2026-09-04 10:35:56 +0100

    melee strength component added

--- src/main/java/com/boundless/registry/StrongestComponents.java
+++ src/main/java/com/boundless/registry/StrongestComponents.java
@@ -6,6 +6,7 @@ import net.minecraft.component.ComponentType;
 public class StrongestComponents {
     public static ComponentType<Float> BLACK_FLASH_CHANCE = DataComponentRegistry.registerComponent("black_flash_chance", builder -> ComponentType.<Float>builder().codec(Codec.FLOAT));
     public static ComponentType<Float> BLACK_FLASH_DAMAGE_MULTIPLIER = DataComponentRegistry.registerComponent("black_flash_damage_multiplier", builder -> ComponentType.<Float>builder().codec(Codec.FLOAT));
+    public static ComponentType<Float> MELEE_STRENGTH = DataComponentRegistry.registerComponent("melee_strength", builder -> ComponentType.<Float>builder().codec(Codec.FLOAT));
 
     public static void initialize() {}
 }
