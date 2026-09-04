a80f78ed86c3238d523fe279ba587fb3aed57bef
j <j@noreply.com>
2026-09-04 23:27:57 +0100

    Renaming player_animation to player_animations to get rid of annoying warnings

--- src/main/java/com/boundless/hero/yuji/technique/abilities/ManjiKickAbility.java
+++ src/main/java/com/boundless/hero/yuji/technique/abilities/ManjiKickAbility.java
@@ -4,12 +4,13 @@ import com.boundless.BoundlessAPI;
 import com.boundless.ability.TechniqueAbility;
 import com.boundless.util.PlayerAnimationUtils;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ManjiKickAbility extends TechniqueAbility {
     @Override
     public void activate(PlayerEntity player) {
-        PlayerAnimationUtils.playSyncedAnimation(player, BoundlessAPI.id("manji_kick_indicator"));
+        PlayerAnimationUtils.playSyncedAnimation(player, BoundlessAPI.id("manji_kick_parry"), 2.0f, false, true, 3000);
     }
 
     @Override
@@ -20,5 +21,10 @@ public class ManjiKickAbility extends TechniqueAbility {
     @Override
     public long getCooldown(PlayerEntity player) {
         return 20;
+    }
+
+    @Override
+    public Text getDisplayText(PlayerEntity player) {
+        return Text.of("Manji Kick");
     }
 }
