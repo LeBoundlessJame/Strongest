bf604037fdff00a171a7e6c046b5190fc5cf0b31
j <j@noreply.com>
2026-09-05 01:43:17 +0100

    Buffed dropkick knockback

--- src/main/java/com/boundless/hero/yuji/technique/abilities/ManjiKickAbility.java
+++ src/main/java/com/boundless/hero/yuji/technique/abilities/ManjiKickAbility.java
@@ -13,7 +13,7 @@ import net.minecraft.util.math.Vec3d;
 public class ManjiKickAbility extends KickAbility {
     public ManjiKickAbility() {
         this.setAnimation(BoundlessAPI.id("manji"));
-        this.setKnockback(new Vec3d(1.1, 1.2f, 1.1));
+        this.setKnockback(new Vec3d(1.4, 1.2f, 1.4));
         this.setAttackDuration(20);
         this.setImpactTick(11);
         this.setAnimationSpeed(1.0f);
